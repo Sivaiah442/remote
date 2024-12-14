71b76f54daedaf88d8c32e7940b9af4aea372049
Sivaiah442 <sivaiahkalavakuri235@gmail.com>
2024-12-14 11:45:11 +0530

    Add files via upload

---
+++ pro3.py
@@ -0,0 +1,11 @@
+e={'enmes':["virat","dhoni","pant","rahu","gill"], 'age':[24,27,28,29,30,31], 'salary':[19000,25000,18000,30000,70000]}
+print("The employyes salaries are",e['salary'])
+for i in e['salary']:
+    print(i)
+L1=e['salary']
+print("the total salary is",sum(L1))
+print("minimum salary is",min(L1))
+print("The maximum salary is",max(L1))
+for v in L1:
+    if i>=25000:
+        print(e['enmes'])
